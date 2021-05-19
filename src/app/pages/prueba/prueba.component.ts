541f9f2e72385887dc05ce87e1cacdfa259820af
paul351 <paulalcca@upeu.edu.pe>
2021-05-19 15:26:13 -0500

    speechSynthesis
    pequeño ejercicio de ingles

--- src/app/pages/prueba/prueba.component.ts
+++ src/app/pages/prueba/prueba.component.ts
@@ -2,55 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { CalendarEvent, CalendarView } from 'angular-calendar';
-import {
-  isSameMonth,
-  isSameDay,
-  startOfMonth,
-  endOfMonth,
-  startOfWeek,
-  endOfWeek,
-  startOfDay,
-  endOfDay,
-  format,
-} from 'date-fns';
-import { Observable } from 'rxjs';
-// const colors: any = {
-//   red: {
-//     primary: '#ad2121',
-//     secondary: '#FAE3E3',
-//   },
-//   blue: {
-//     primary: '#1e90ff',
-//     secondary: '#D1E8FF',
-//   },
-//   yellow: {
-//     primary: '#e3bc08',
-//     secondary: '#FDF1BA',
-//   },
-// };
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import Firebase from 'firebase/app';
+import Swal from 'sweetalert2'
 
-// interface Film {
-//   id: number;
-//   title: string;
-//   release_date: string;
-// }
 
-// function getTimezoneOffsetString(date: Date): string {
-//   const timezoneOffset = date.getTimezoneOffset();
-//   const hoursOffset = String(
-//     Math.floor(Math.abs(timezoneOffset / 60))
-//   ).padStart(2, '0');
-//   const minutesOffset = String(Math.abs(timezoneOffset % 60)).padEnd(2, '0');
-//   const direction = timezoneOffset > 0 ? '-' : '+';
-
-//   return `T00:00:00${direction}${hoursOffset}:${minutesOffset}`;
-// }
+const data = [{ text: "how are you?" }, { text: "hello" }, { text: "my name is alexa" }, { text: "who is he?" }, { text: "have a nice day" }, { text: "what did you say?" },
+{ text: "what is your name?"}, { text: "Do you speak Spanish?"}];
 
 @Component({
   selector: 'app-prueba',
   templateUrl: './prueba.component.html',
   styleUrls: ['./prueba.component.less']
 })
+
 export class PruebaComponent implements OnInit {
 
   // view: CalendarView = CalendarView.Month;
@@ -60,83 +26,52 @@ export class PruebaComponent implements OnInit {
   // events$: Observable<CalendarEvent<{ film: Film }>[]>;
 
   // activeDayIsOpen: boolean = false;
-
-  // constructor(private http: HttpClient) {}
+  textToSpeach: string = "";
+  diseibol: boolean = false;
+  inp_answer: string = "";
+  user: Firebase.User;
+  constructor(private router: Router,
+    private afAuth: AngularFireAuth,) { }
 
   ngOnInit(): void {
-  //   this.fetchEvents();
+    this.afAuth.user.subscribe(user => {
+      if (user) {
+        this.user = user;
+        if (user.emailVerified == false) {
+          this.router.navigate(['/emailVerification']);
+        }
+      } else {
+        this.router.navigate(['/login']);
+      }
+    });
+    this.reset();
   }
 
-  // fetchEvents(): void {
-  //   const getStart: any = {
-  //     month: startOfMonth,
-  //     week: startOfWeek,
-  //     day: startOfDay,
-  //   }[this.view];
+  reset() {
+    this.diseibol = true;
+    this.inp_answer = "";
+    let value = (Math.floor(Math.random() * data.length));
+    this.textToSpeach = data[value].text.toLowerCase();
+    console.log(this.textToSpeach);
 
-  //   const getEnd: any = {
-  //     month: endOfMonth,
-  //     week: endOfWeek,
-  //     day: endOfDay,
-  //   }[this.view];
+  }
 
-  //   const params = new HttpParams()
-  //     .set(
-  //       'primary_release_date.gte',
-  //       format(getStart(this.viewDate), 'yyyy-MM-dd')
-  //     )
-  //     .set(
-  //       'primary_release_date.lte',
-  //       format(getEnd(this.viewDate), 'yyyy-MM-dd')
-  //     )
-  //     .set('api_key', '0ec33936a68018857d727958dca1424f');
+  voice(): void {
+    var msg = new SpeechSynthesisUtterance();
+    msg.lang = 'en-US';
+    msg.text = this.textToSpeach;
+    speechSynthesis.speak(msg);
+    this.diseibol = false;
+  }
 
-  //   this.events$ = this.http
-  //     .get('https://api.themoviedb.org/3/discover/movie', { params })
-  //     .pipe(
-  //       map(({ results }: { results: Film[] }) => {
-  //         return results.map((film: Film) => {
-  //           return {
-  //             title: film.title,
-  //             start: new Date(
-  //               film.release_date + getTimezoneOffsetString(this.viewDate)
-  //             ),
-  //             color: colors.yellow,
-  //             allDay: true,
-  //             meta: {
-  //               film,
-  //             },
-  //           };
-  //         });
-  //       })
-  //     );
-  // }
+  answer(): void {
+    let answer = this.inp_answer.toLowerCase();
 
-  // dayClicked({
-  //   date,
-  //   events,
-  // }: {
-  //   date: Date;
-  //   events: CalendarEvent<{ film: Film }>[];
-  // }): void {
-  //   if (isSameMonth(date, this.viewDate)) {
-  //     if (
-  //       (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
-  //       events.length === 0
-  //     ) {
-  //       this.activeDayIsOpen = false;
-  //     } else {
-  //       this.activeDayIsOpen = true;
-  //       this.viewDate = date;
-  //     }
-  //   }
-  // }
-
-  // eventClicked(event: CalendarEvent<{ film: Film }>): void {
-  //   window.open(
-  //     `https://www.themoviedb.org/movie/${event.meta.film.id}`,
-  //     '_blank'
-  //   );
-  // }
+    if (answer == this.textToSpeach) {
+      Swal.fire("Good job!", "", "success");
+    } else {
+      Swal.fire("Oh no!", "", "error");
+    }
+  }
 
 }
